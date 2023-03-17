d32496e4290a2bc0a1289256bad92d8edb8851d7
rafo18 <rafaelbarreto10@hotmail.com>
2023-03-16 21:37:21 -0400

    dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer")
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
@@ -13,12 +14,13 @@ async function setupNodeEvents(on, config) {
       plugins: [createEsbuildPlugin.default(config)],
     })
   );
-
+    allureWriter(on,config)
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
 }
 
 module.exports = defineConfig({
+  projectId: 'mzsemf',
   e2e: {
     specPattern: "**/*.feature",
     setupNodeEvents,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-xpath'
+import "@shelex/cypress-allure-plugin"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
