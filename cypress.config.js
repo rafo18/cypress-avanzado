5e51da976a8deecaffb42275a24caa24c84f547a
rafo18 <rafaelbarreto10@hotmail.com>
2023-03-16 21:50:24 -0400

    dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
+  projectId: 'mzsemf',
   e2e: {
     baseUrl:"https://pokedexpokemon.netlify.app/",
     setupNodeEvents(on, config) {
